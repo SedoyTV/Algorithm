cc8246f39f45cc6b4dc2779f21d7b680d063dbf6
sedoy <sedoy.tv@internet.ru>
2024-05-21 23:39:53 +0300

    final fix setup

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ docker-compose exec php composer install
 
 docker-compose exec php php artisan key:generate
 
-docker-compose exec chown -R www-data:www-data /var/www/html/storage
+docker-compose exec php chown -R www-data:www-data /var/www/html/storage
 
-docker-compose exec chmod -R 777 /var/www/html/storage
+docker-compose exec php chmod -R 777 /var/www/html/storage
 
-docker-compose exec chmod -R 777 /var/www/html/setup.sh
+docker-compose exec php chmod -R 777 /var/www/html/setup.sh
 
 echo "Установка завершена"
