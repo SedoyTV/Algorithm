1d58883f9adf584d3c0953ab1a6e4d2ad69d9862
sedoytv <sedoy.tv@internet.ru>
2024-05-22 15:33:12 +0300

    rework api.php

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\SymbolCountController;
-use App\Http\Controllers\MinNumberController;
-use App\Http\Controllers\OddSumController;
-use App\Http\Controllers\DeclensionController;
+use App\Http\Controllers\
+{
+    SymbolCountController,
+    MinNumberController,
+    OddSumController,
+    DeclensionController
+};
 
-Route::post('/declension', [DeclensionController::class, 'handleRequest']);
-Route::post('/count-symbol', [SymbolCountController::class, 'handleRequest']);
-Route::post('/min-number', [MinNumberController::class, 'handleRequest']);
-Route::post('/odd-sum', [OddSumController::class, 'handleRequest']);
+$controllers = [
+    '/declension' => DeclensionController::class,
+    '/count-symbol' => SymbolCountController::class,
+    '/min-number' => MinNumberController::class,
+    '/odd-sum' => OddSumController::class,
+];
+
+foreach ($controllers as $uri => $controller) {
+    Route::post($uri, [$controller, 'handleRequest']);
+}
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
